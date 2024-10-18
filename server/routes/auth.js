b0695265c5e6481711a212fc6653db494a979ce1
Kishore298 <kishore103345@gmail.com>
2024-10-18 18:57:36 +0530

    implementes user authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../controllers/authcontroller');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
